7bb5c6e15c8512dfd8454684aab52d5a15251cbf
Anil Kumar K K <akumarkk@outlook.com>
2024-12-06 20:15:25 -0800

    gh actions

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    // Replace these with your Firebase config values
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+};
+
+
+
+export default firebaseConfig;
